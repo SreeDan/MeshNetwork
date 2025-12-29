8d8ee2e35bc326c2ee5e128343fb070456620480
sreekara <sreedansd@gmail.com>
2025-12-29 02:26:02 -0500

    refactor(rpc): removed old rpc manager handlers

--- src/rpc/handlers/TextHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "IRpcMessageHandler.h"
-
-class TextHandler : public IRpcMessageHandler {
-public:
-    void handle(std::shared_ptr<RpcConnection> conn, const mesh::Envelope &env) override;
-};
-
